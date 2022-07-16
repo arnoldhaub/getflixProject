2ec6716d6d28d62528204225d9b5b47cb112a86d
guillaume1060 <breyerguillaume@gmail.com>
2022-07-16 13:44:27 +0200

    update connect.php

--- docker_env/application/source/src/connect.php
+++ docker_env/application/source/src/connect.php
@@ -1,5 +1,5 @@
 <?php
 
-    $db = new PDO('mysql:host=database;dbname=getflix;charset=utf8', 'root', 'root');
+    $db = new PDO('mysql:host=sql11.freesqldatabase.com;dbname=sql11506911;charset=utf8', 'sql11506911', 'YD2lmRk5Uz');
 
 ?>

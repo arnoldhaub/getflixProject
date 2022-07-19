eaebcdecd2a3589752e4e6fda12eca2fc7c26183
Arnold Haubursin <arnoldhaubursin@gmail.com>
2022-07-19 14:57:37 +0200

    Adding API to home.php

--- docker_env/application/source/api/api/movies.php
+++ docker_env/application/source/api/api/movies.php
@@ -9,3 +9,37 @@ curl_setopt($ct, CURLOPT_HTTPHEADER, array("Accept: application/json"));
 $response = curl_exec($ct);
 curl_close($ct);
 $movies = json_decode($response);
+
+// DERNIER FILMS SCIENCE-FICTION
+$ct = curl_init();
+curl_setopt($ct, CURLOPT_URL, "https://api.themoviedb.org/3/movie/now_playing?api_key=$key&language=en-US&region=US&with_genres=878"); // URL reprenant film SF
+curl_setopt($ct, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ct, CURLOPT_HEADER, FALSE);
+curl_setopt($ct, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+$response = curl_exec($ct);
+curl_close($ct);
+$moviesLatest = json_decode($response);
+
+// TOP-RATED FILMS SCIENCE-FICTION
+$ct = curl_init();
+curl_setopt($ct, CURLOPT_URL, "https://api.themoviedb.org/3/movie/top_rated?api_key=$key&with_genres=878"); // URL reprenant film SF
+curl_setopt($ct, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ct, CURLOPT_HEADER, FALSE);
+curl_setopt($ct, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+$response = curl_exec($ct);
+curl_close($ct);
+$moviesTopRated = json_decode($response);
+
+// POPULAR FILMS SCIENCE-FICTION
+$ct = curl_init();
+curl_setopt($ct, CURLOPT_URL, "https://api.themoviedb.org/3/movie/popular?api_key=$key&with_genres=878"); // URL reprenant film SF
+curl_setopt($ct, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ct, CURLOPT_HEADER, FALSE);
+curl_setopt($ct, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+$response = curl_exec($ct);
+curl_close($ct);
+$moviesPopular = json_decode($response);
+
+
+
+
--- docker_env/application/source/api/api/series.php
+++ docker_env/application/source/api/api/series.php
@@ -10,3 +10,33 @@ $response = curl_exec($ct);
 curl_close($ct);
 $series = json_decode($response);
 
+// DERNIER FILMS SCIENCE-FICTION
+$ct = curl_init();
+curl_setopt($ct, CURLOPT_URL, "https://api.themoviedb.org/3/discover/tv?api_key=$key&language=en-US&sort_by=first_air_date.desc&with_genres=10765&with_original_language=en&watch_region=us"); // URL reprenant film SF
+curl_setopt($ct, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ct, CURLOPT_HEADER, FALSE);
+curl_setopt($ct, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+$response = curl_exec($ct);
+curl_close($ct);
+$seriesLatest = json_decode($response);
+
+// TOP-RATED FILMS SCIENCE-FICTION
+$ct = curl_init();
+curl_setopt($ct, CURLOPT_URL, "https://api.themoviedb.org/3/tv/top_rated?api_key=$key&with_genres=10765"); // URL reprenant film SF
+curl_setopt($ct, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ct, CURLOPT_HEADER, FALSE);
+curl_setopt($ct, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+$response = curl_exec($ct);
+curl_close($ct);
+$seriesTopRated = json_decode($response);
+
+// POPULAR FILMS SCIENCE-FICTION
+$ct = curl_init();
+curl_setopt($ct, CURLOPT_URL, "https://api.themoviedb.org/3/tv/popular?api_key=$key&with_genre=10765"); // URL reprenant film SF
+curl_setopt($ct, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ct, CURLOPT_HEADER, FALSE);
+curl_setopt($ct, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+$response = curl_exec($ct);
+curl_close($ct);
+$seriesPopular = json_decode($response);
+

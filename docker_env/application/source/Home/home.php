bd0b7cfa603241911f713193cbf6af6f063afc9f
Arnold Haubursin <arnoldhaubursin@gmail.com>
2022-07-19 15:12:09 +0200

    adding footer

--- docker_env/application/source/Home/home.php
+++ docker_env/application/source/Home/home.php
@@ -194,17 +194,19 @@
 
         <footer>
             <div class="footer_div">
-                <img class="logo_bottom" src="images/logo.svg" alt="logo">
+                <img class="logo_bottom" src="images/logo.svg" alt="logo"> 
+                </div>
+            <div>
+                <img class="logo_resp_bottom" src="images/logo_planete_resp.svg" alt="logo">
             </div>
-            <div class="span_div">
+           <div class="span_div">
                 <span class="span_footer">SCI-FI STREAMING SOLUTION</span>
                 <span class="span_footer">CREDITS & COOKIES</span>
-                <span class="span_footer">NOVA 2022:copyright:</span>
+                <span class="span_footer">NOVA 2022©</span>
                 <span class="span_footer">ACCESSIBILITY</span>
                 <span class="span_footer">REPORT A BUG</span>
             </div>
-            </div>
-        </footer>
+        </footer> 
 
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');

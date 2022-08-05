1dd03c37723d7599859f7500c9129cc5c023f927
Arnold Haubursin <arnoldhaubursin@gmail.com>
2022-08-05 16:29:39 +0200

    title correction

--- docker_env/application/source/Home/home.php
+++ docker_env/application/source/Home/home.php
@@ -58,7 +58,7 @@
                 <div class="carousel-item active">
                     <img class="d-block w-100" <?php echo 'src="'.$imgurl.$latestMovie->results[0]->backdrop_path.'" alt="'.$imgurl.$latestMovie->results[0]->original_title.'"';?>>
                     <div class="carousel-caption d-none d-md-block">
-                        <h5><?php echo $latestMovie->results[0]->original_title; ?></h5>
+                        <h5><?php echo $latestMovie->results[0]->title; ?></h5>
                         <p><?php echo substr($latestMovie->results[0]->overview, 0,250 )." (...)"; 
                             if(strlen($latestMovie->results[1]->overview) > 250)
                                     { echo '(...)';} ?></p>
@@ -67,7 +67,7 @@
                 <div class="carousel-item">
                     <img class="d-block w-100" <?php echo 'src="'.$imgurl.$latestMovie->results[1]->backdrop_path.'" alt="'.$imgurl.$latestMovie->results[1]->original_title.'"';?>>
                     <div class="carousel-caption d-none d-md-block">
-                        <h5><?php echo $latestMovie->results[1]->original_title; ?></h5>
+                        <h5><?php echo $latestMovie->results[1]->title; ?></h5>
                         <p><?php echo substr($latestMovie->results[1]->overview, 0,250 );
                             if(strlen($latestMovie->results[1]->overview) > 250)
                                     { echo '(...)';} ?></p>
@@ -76,7 +76,7 @@
                 <div class="carousel-item">
                     <img class="d-block w-100" <?php echo 'src="'.$imgurl.$latestMovie->results[2]->backdrop_path.'" alt="'.$imgurl.$latestMovie->results[2]->original_title.'"';?>>
                     <div class="carousel-caption d-none d-md-block">
-                        <h5><?php echo $latestMovie->results[2]->original_title; ?></h5>
+                        <h5><?php echo $latestMovie->results[2]->title; ?></h5>
                         <p><?php echo substr($latestMovie->results[2]->overview, 0,250 )." (...)";
                             if(strlen($latestMovie->results[2]->overview) > 250)
                             { echo '(...)';} ?></p>

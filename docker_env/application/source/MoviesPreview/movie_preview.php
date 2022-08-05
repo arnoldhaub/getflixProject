a002680cb9be754e98a53fdd501b8a14c7abb43b
Arnold Haubursin <arnoldhaubursin@gmail.com>
2022-08-05 13:52:49 +0200

    Movie preview -without video

--- docker_env/application/source/MoviesPreview/movie_preview.php
+++ docker_env/application/source/MoviesPreview/movie_preview.php
@@ -1,18 +1,33 @@
-<!DOCTYPE html>
+<?php
 
+//======================================================================
+// SECURITY - ID VERIFICATION // BACK TO MOVIES INDEX
+//======================================================================
+
+if (empty($_GET['id'])) {
+    header("location: index.php");
+}
+
+
+//======================================================================
+// FETCH ALL INFORMATION FROM THE API && PUT ID IN VARIABLE
+//======================================================================
+
+$id = $_GET['id'];
+include "../api/api/info.php";
+ 
+?>
+<!DOCTYPE html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edege">
         <title>Movie</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"  media="screen">
-  <link href="./styles/styles_movie_preview.css" rel="stylesheet">
-        <link href="./styles/styles_nav_footer.css" rel="stylesheet">
-        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"  media="screen">
+        <link href="./styles/styles_movie_preview.css" rel="stylesheet">
+        <link href="./styles/styles_nav_footer.css" rel="stylesheet"> 
     </head>
-
-
     <body>  
         <header>
             <img class="logo" src="../images/logo.svg" alt="logo">
@@ -41,101 +56,94 @@
                 <img class="userImage" name="userImage" src="../images/CN.jpg" alt="userImage">
             </div>
         </header>
- <div class="BackgroundImage">
-                    <img src="../images/Only-Murders-In-The-Building-Star-Plus.jpeg" id="testImage" style="max-width: 100%;">
-                    <div class="headerMovie">
-   
-                                       <div class="nameMovie">
-                                           <h1>Movie's Name</h1>
-                                       </div>
+        
+        <div class="BackgroundImage">
+                <img src="<?php echo $imgurl.$infoMovie->backdrop_path;?>" id="testImage" style="max-width: 100%;">
+                <div class="headerMovie">
 
-
-                                        <div class="infosMovie">
-                                                          <button disabled>18+</button>
-                                                          <button disabled>VO</button>
-                                                          <button disabled>VOSTFR</button>
-                                                          <p> 2019 - 2 Saisons
-                                                          </p>
-                                        </div>
-
-                                        <div class="typeMovie">
-                                            <p class="typeMovieDescription">Drames, Thriller, Distopie</p>
-                                        </div>
-
-
-                                         <div class="buttonsMovie">
-                                             <button class="play" id="playButton"><i class="fa-solid fa-play" id="fa-play"></i>LECTURE</button>
-                                         </div>
-
-
-                                        <div class="descriptionMovie">
-                                            <p class="description"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid cumque suscipit animi, nihil tenetur quidem! Nihil eius optio quas ex assumenda voluptate, magnam quod. Eius quis illum architecto quisquam nihil.</p>
-                                            <p class="empty"></p>
-                                        </div>
-
-    
+                    <div class="nameMovie">
+                        <h1><?php echo $infoMovie->title; ?></h1>
                     </div>
-</div> 
 
+                    <div class="infosMovie">
+                        <button disabled>18+</button>
+                        <button disabled>VO</button>
+                        <button disabled>VOSTFR</button>
+                        <p><?php echo substr($infoMovie->release_date, 0, 4); ?></p> 
+                    </div>
 
-    
-    <div class="container_movie">
-        <div class="container">
-            <p class="title_slide">Vous aimerez peut-etre...</p>  
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
+                    <div class="typeMovie">
+                        <p class="typeMovieDescription">
+                            <?php 
+                            
+                            foreach($infoMovie->genres as $i){
+                                if ($i === end($infoMovie->genres)) {
+                                    echo $i->name;
+                                }
+                                else{
+                                    // not last element
+                                    echo $i->name.', ' ;
+                                }
+                            }?>
+                        </p>
+                    </div>
 
-                    <?php
-                    include "./../api/api/info.php";
-                    foreach ($moviesLatest->results as $p) { // RECENT SF MOVIE
-                        if (!empty($p->poster_path)) {
-                            echo  "<div class='swiper-slide'>
-                            <a href='movie.php?id=" . $p->id . "'><img src='" . $imgurl_500 . $p->poster_path . "'></a>
-                        </div>";
-                        }
-                    } ?>
+                    <div class="buttonsMovie">
+                        <button class="play" id="playButton" data-toggle="modal" data-target="#watch2"><i class="fa-solid fa-play" id="fa-play"></i>LECTURE</button>
+                    </div>
+                    <!-- MODAL  -->
+                    <div id="watch2" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                </div>
+                                <div class="modal-body">
+                                    <iframe id="watch3" src="https://www.youtube.com/embed/<?php echo $infoMovie->videos->results[0]->key;?>" allow="autoplay;" allowfullscreen>
+                                    </iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
+                    <div class="descriptionMovie">
+                        <p class="description"><?php echo $infoMovie->overview ?></p>
+                        <p class="empty"></p>
+                    </div>
                 </div>
-                <!-- Add Arrows -->
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
-            </div>
+        </div> 
+
+        <div class="container_movie">
+            <div class="container">
+                <p class="title_slide">You are going to like...</p>  
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+
+                        <?php
+                        foreach ($moviesRecommandations->results as $p) { 
+                            if (!empty($p->poster_path)) {
+                                echo  "<div class='swiper-slide'>
+                                <a href='movie_preview.php?id=" . $p->id . "'><img src='" . $imgurl_500 . $p->poster_path . "'></a>
+                            </div>";
+                            }
+                        } ?>
+
+                    </div>
+                    <!-- Add Arrows -->
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
+                </div>
+            </div>         
         </div>
         
-        <div class="container">
-            <p class="title_slide">Vous aimerez peut-etre...</p>  
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-
-                    <?php
-                    include "./../api/api/info.php";
-                    foreach ($moviesLatest->results as $p) { // RECENT SF MOVIE
-                        if (!empty($p->poster_path)) {
-                            echo  "<div class='swiper-slide'>
-                            <a href='movie.php?id=" . $p->id . "'><img src='" . $imgurl_500 . $p->poster_path . "'></a>
-                        </div>";
-                        }
-                    } ?>
-
-                </div>
-                <!-- Add Arrows -->
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
-            </div>
-        </div>
-
-                </div>
-        
-
-
         <footer>
             <div class="footer_div">
                 <img class="logo_bottom" src="../images/logo.svg" alt="logo"> 
-                </div>
+            </div>
             <div>
                 <img class="logo_resp_bottom" src="../images/logo_planete_resp.svg" alt="logo">
             </div>
-           <div class="span_div">
+            <div class="span_div">
                 <span class="span_footer">SCI-FI STREAMING SOLUTION</span>
                 <span class="span_footer">CREDITS & COOKIES</span>
                 <span class="span_footer">NOVA 2022©</span>
@@ -143,24 +151,15 @@
                 <span class="span_footer">REPORT A BUG</span>
             </div>
         </footer>
-        
-        
-
-
-
-
-        
-
-
 
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
         </style>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.5/swiper-bundle.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="./movie_preview_script.js"></script>
-<script src="/Home/script.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.5/swiper-bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="./movie_preview_script.js"></script>
+        <script src="/Home/script.js"></script>
     </body>
 </html>

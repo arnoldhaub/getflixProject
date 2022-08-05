e26a48f8a77483b304f1d482610ca0615d4a002b
AnthonyVanDeyck <anthonyvandeyck@gmail.com>
2022-08-05 12:25:18 +0200

    com fonctionnel plus cas merge avec l'api

--- docker_env/application/source/MoviesPreview/com.php
+++ docker_env/application/source/MoviesPreview/com.php
@@ -2,19 +2,21 @@
 ///////////////////////////////////////connexion DB\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 include('../src/connect.php');
 /////////////////////////////////////////////code\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-if(!empty($_POST['comment']))
-{
-    $commentaire =htmlspecialchars($_POST['comment']);
-    $pseudo = htmlspecialchars($_GET['pseudo']);
-    $id_film = $_GET['id'];
-
-    $push = $db ->prepare("INSERT INTO comments(id_film, pseudo, commentaires VALUES (?, ?, ?)");
-    $push-> execute(array($id_film, $pseudo, $_POST['comment']));
-}
-
 $request = $db ->query('SELECT * FROM comments /*WHERE id_film = ?*/');
 $request ->execute();
 $comment = $request->fetchAll();
+
+if(isset($_POST['commentaires'], $_POST['pseudo'], $_POST['id']))
+{
+    $commentaire =htmlspecialchars($_POST['commentaires']);
+    $pseudo = htmlspecialchars($_POST['pseudo']);
+    $id_film = htmlspecialchars($_POST['id']);
+
+    $push = $db ->prepare('INSERT INTO `comments` (`id`, `id_film`, `pseudo`, `commentaires`, `date`) VALUES (NULL, ?, ?, ?, CURRENT_TIMESTAMP)');
+    $push->execute(array($id_film, $pseudo, $commentaire));
+    header('location: com.php');
+}
+
 ?>
 <div name="com">
    <?php
@@ -26,7 +28,9 @@ $comment = $request->fetchAll();
 </div>
 <div name="form_com">
     <form method="POST">
-        <input type="text" placeholder="votre commentaire"><br/> 
+        <input type="text" placeholder="pseudo" name="pseudo"><br/>
+        <input type="text" placeholder="votre commentaire" name="commentaires"><br/> 
+        <input type="text" placeholder="id_film" name="id"><br/>
         <button type="submit">envoyer</button>
     </form>
 </div>

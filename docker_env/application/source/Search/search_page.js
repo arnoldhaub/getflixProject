049187f92b046d3a15d32cc89c2847cba3a70798
Timur Karamenderes <timurkaramenderes@babings.studio>
2022-08-08 14:39:51 +0200

    Update search_page.js
    last changes on search_page.js

--- docker_env/application/source/Search/search_page.js
+++ docker_env/application/source/Search/search_page.js
@@ -1,8 +1,4 @@
-/* ========================================== 
 
-         ↓↓↓↓  FUNCTION HOVER ↓↓↓↓↓
-
-============================================== */
 
 const SelectCircle = document.getElementById("circle_selected");
 SelectCircle.hidden = true;
@@ -10,9 +6,21 @@ const SelectCircle2 = document.getElementById("circle_selected2");
 SelectCircle2.hidden = true;
 const SelectCircle3 = document.getElementById("circle_selected3");
 SelectCircle3.hidden = true;
-const SearchBar = document.getElementById("container-content");
-SearchBar.hidden = true;
+//const SearchBar = document.getElementById("container-content");
+//SearchBar.hidden = true;
 
+
+
+
+ if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+};
+
+/* ========================================== 
+
+         ↓↓↓↓  FUNCTION HOVER ↓↓↓↓↓
+
+============================================== */
 
 $(document).ready(function(){
 
@@ -78,6 +86,7 @@ $(document).ready(function(){
 ============================================== */
 
 
+
 $(document).ready(function () {
 
 
@@ -86,6 +95,16 @@ $(document).ready(function () {
    elem.css(fade).slideUp();
    }; // fonction qui permet le fade OUT et qui slide up le menu
    
+    const FadeOnlyOut = (elem) => {
+    const fadeOpacity = { opacity: 0, transition: 'opacity 300ms ease-in-out' };
+        elem.css(fadeOpacity);
+    }
+    
+    const FadeOnlyIn = (elem) => {
+    const fadeOpacityIn = { opacity: 100, transition: 'opacity 700ms ease-in-out' };
+        elem.css(fadeOpacityIn);
+    }
+
    
    const slideFadeIn = (elem) => {
    const fadeIn = { opacity: 100, transtion: 'opacity 200ms ease-in-out' };
@@ -102,24 +121,28 @@ $(document).ready(function () {
    	 	if (clickCounter % 2 == 0){
    			$("div.container_menu > p:not(.choix2)").slideUp();
             SelectCircle.hidden = false;
+            FadeOnlyIn($("#container-content"));
             $("#circle_selector").hide();
-            SearchBar.hidden = false;
-            slideFadeIn($("container-content"));
+            $("#input_Search").prop('disabled', false);
             $("#movie").prop("checked", true); // permet de check le radio button onclick
             slideFade($("div.container_menu > p:not(.choix2)"));
-            
+            $("#input_Search").focus(function () {
+            $(".wrapper_container_search > i").css('color', 'white');  
+            });
+            $("#input_Search").focusout(function () {
+            $(".wrapper_container_search > i").css('color', 'gray');  
+});
     		clickCounter++;
     		console.log(clickCounter);
-    
-            } else {
-            SelectCircle.hidden = true;
+                  } else {
             
-            slideFade($("container-content"));
+            SelectCircle.hidden = true;          
+            FadeOnlyOut($("#container-content"));
+            $("#input_Search").prop('disabled', true);          
             $("#circle_selector").show();
             $("#movie").prop("checked", true); // permet de check le radio button onclick
     		$("div.container_menu > p").slideDown();
             slideFadeIn($("div.container_menu > p:not(.choix2)"));
-            
     		clickCounter++;
     		console.log(clickCounter);
     		};
@@ -132,16 +155,26 @@ $(document).ready(function () {
    $(".choix3").click(function(){
        if (clickCounter % 2 == 0) {
             $("#serie").prop("checked", true); // permet de check le radio button onclick
-           $("div.container_menu > p:not(.choix3)").slideUp(300);
+            $("div.container_menu > p:not(.choix3)").slideUp(300);
             SelectCircle2.hidden = false;
+            FadeOnlyIn($("#container-content"));
+            $("#input_Search").prop('disabled', false);
             $("#circle_selector2").hide();
-            slideFade($("div.container_menu > p:not(.choix3)"));
+           slideFade($("div.container_menu > p:not(.choix3)"));
+            $("#input_Search").focus(function () {
+            $(".wrapper_container_search > i").css('color', 'white');  
+            });
+            $("#input_Search").focusout(function () {
+            $(".wrapper_container_search > i").css('color', 'gray');  
+});
     		clickCounter++;
     		console.log(clickCounter);
     
        } else {
             SelectCircle2.hidden = true;
             $("#circle_selector2").show();
+            FadeOnlyOut($("#container-content"));
+            $("#input_Search").prop('disabled', true);
             $("#serie").prop("checked", true);
     		$("div.container_menu > p").slideDown();
             slideFadeIn($("div.container_menu > p:not(.choix3)"));
@@ -157,13 +190,24 @@ $(document).ready(function () {
             $("div.container_menu > p:not(.choix4)").slideUp();
             SelectCircle3.hidden = false;
             $("#circle_selector3").hide();
+            FadeOnlyIn($("#container-content"));
+            $("#input_Search").prop('disabled', false);
             slideFade($("div.container_menu > p:not(.choix4)"));
+            $("#input_Search").focus(function () {
+            $(".wrapper_container_search > i").css('color', 'white');  
+            });
+            $("#input_Search").focusout(function () {
+            $(".wrapper_container_search > i").css('color', 'gray');  
+});
     		clickCounter++;
-    		console.log(clickCounter);
+            console.log(clickCounter);
+                      
     
             } else {
             SelectCircle3.hidden = true;
             $("#circle_selector3").show();
+            FadeOnlyOut($("#container-content"));
+            $("#input_Search").prop('disabled', true);
     		$("div.container_menu > p").slideDown();
             slideFadeIn($("div.container_menu > p:not(.choix4)"));
     		clickCounter++;

126e2ade0067b63d6c5f83c493afc772ae7a687f
guillaume1060 <breyerguillaume@gmail.com>
2022-08-09 13:02:18 +0200

    ajout header sur serie.php

--- docker_env/application/source/MoviesPreview/serie.php
+++ docker_env/application/source/MoviesPreview/serie.php
@@ -78,33 +78,9 @@ include "../api/api/info.php";
 <!-----------------------------------------------------------------------
                      HEADER + MENU
 ------------------------------------------------------------------------->
-        <header>
-            <img class="logo" src="../images/logo.svg" alt="logo">
-            <img class="logo_minia" src="../images/logo_planete.svg" alt="logo_minia">
-            <nav>
-                <ul class="nav_links">
-                    <li><a href="/Home/home.php"> HOME</a></li>
-                    <li><a href="#">FILMS</a></li>
-                    <li><a href="#">SERIES</a></li>
-                    <li><a href="#"></a>RECHERCHE</a></li>
-                </ul>
-
-                <ul class="nav_links_responsive">
-                    <li><a href="#"><i class="fa-solid fa-house"></i></a></li>
-                    <li><a href="#"><i class="fa-solid fa-film"></i></i></a></li>
-                    <li><a href="#"><i class="fa-solid fa-tv"></i></i></a></li>
-                    <li><a href="#"></a><i class="fa-solid fa-magnifying-glass"></i></i></a></li>
-                </ul>
-            </nav>
-
-            <!--   <div class="searchbar">
-                        <input type="text" name="searchBar" placeholder="Search"><i class="fa-solid fa-magnifying-glass"></i>
-            </div> -->
-            <div class="user">
-                <p class="p_username">USER</p>
-                <img class="userImage" name="userImage" src="../images/CN.jpg" alt="userImage">
-            </div>
-        </header>
+<?php   
+    include('header_movie.php');
+    ?>
 <!-----------------------------------------------------------------------
                      WEBSITE
 ------------------------------------------------------------------------->

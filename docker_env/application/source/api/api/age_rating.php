477cfdb0c2c3b75cba623afa1994a2348cdfd3c1
Arnold Haubursin <arnoldhaubursin@gmail.com>
2022-08-11 12:31:30 +0200

    section for kids

--- docker_env/application/source/api/api/age_rating.php
+++ docker_env/application/source/api/api/age_rating.php
@@ -22,3 +22,25 @@ if(isset($id, $key)) {
     curl_close($ct);
     $serieAgeRating = json_decode($response, true);
 }
+
+if(isset($key, $page)) {
+    $ct = curl_init();
+    curl_setopt($ct, CURLOPT_URL, "https://api.themoviedb.org/3/discover/movie?api_key=$key&certification_country=US&certification.lte=G&with_genres=878,16&include_adult=false&sort_by=popularity.desc&include_video=true&page=$page"); 
+    curl_setopt($ct, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ct, CURLOPT_HEADER, FALSE);
+    curl_setopt($ct, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+    $response = curl_exec($ct);
+    curl_close($ct);
+    $movieForKids = json_decode($response);
+}
+
+if(isset($key, $page)) {
+    $ct = curl_init();
+    curl_setopt($ct, CURLOPT_URL, "https://api.themoviedb.org/3/discover/movie?api_key=$key&certification_country=US&certification.lte=G&with_genres=878,16&include_adult=false&sort_by=popularity.desc&include_video=true&page=$page"); 
+    curl_setopt($ct, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ct, CURLOPT_HEADER, FALSE);
+    curl_setopt($ct, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+    $response = curl_exec($ct);
+    curl_close($ct);
+    $serieForKids = json_decode($response);
+}

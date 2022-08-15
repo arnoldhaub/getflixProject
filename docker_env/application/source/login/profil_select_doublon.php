718a46901c8c1f42010e3936a6d654c9e6629669
Arnold Haubursin <arnoldhaubursin@gmail.com>
2022-08-15 20:54:15 +0200

    profil select - TITLE

--- docker_env/application/source/login/profil_select_doublon.php
+++ docker_env/application/source/login/profil_select_doublon.php
@@ -22,6 +22,7 @@
 
 <head>
     <meta charset="utf-8">
+    <title>Select a profile</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <link href="./styles/profil_select.css" rel="stylesheet">

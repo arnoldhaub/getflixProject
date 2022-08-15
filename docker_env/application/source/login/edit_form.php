727d0cce3ff62cbcd7049bd35ca05fb0064db59f
Arnold Haubursin <arnoldhaubursin@gmail.com>
2022-08-15 17:39:07 +0200

    modif profil

--- docker_env/application/source/login/edit_form.php
+++ docker_env/application/source/login/edit_form.php
@@ -1,24 +1,37 @@
-
-
-
 <?php
+    $id = $_GET['id'];
 
-
-
-	if (!empty($_POST['pseudo']))
+	if (!empty($_GET['id']))
 	{
+        // Connexion à la base de données
+        require('../src/connect.php');
+        $requete = $db->query("SELECT id_pseudo, pseudo, categorie, email, image FROM profile WHERE id_pseudo='$id'");
+        $infosProfil = $requete->fetch();
+
+        
+//--------- MISE À JOURS DES INFORMATIONS DE LA BDD ---------//
+
+        if (!empty($_POST['pseudo'])){
 		include('../src/connect.php');
-		// variables
+
+		// On reprend les variables du form
 		$pseudo = htmlspecialchars($_POST['pseudo']);
         $categorie = htmlspecialchars($_POST['categorie']);
-        $image = htmlspecialchars($_POST['brandtype']);
 		
-		// // sending (ajouter les droits/accès Adulte ou enfant)
-		$req = $db->prepare("INSERT INTO profile(pseudo, categorie, email, image) VALUES (?, ?, ?, ?)");
-        $req->execute(array($pseudo, $categorie, $_GET['email'],$image));
+		// Modification de la base de données
+        
+		$updateProfil = "UPDATE profile SET pseudo='$pseudo', categorie='$categorie' WHERE id_pseudo='$id'";
+        $updateExecution = $db->exec($updateProfil);
+        
+        // // Mise à jour des informations
+        // $requete = $db->query("SELECT id_pseudo, pseudo, categorie, email, image FROM profile WHERE id_pseudo='$id'");
+        // $infosProfil = $requete->fetch();
 
-        header('location: profil_select.php?email=' . $_GET['email'] . '');
-        }
+    
+		
+        header('location: profil_select_doublon.php?email=' . $infosProfil['email'] . '');
+	    }
+    }
 
 ?>
 <!DOCTYPE html>
@@ -27,134 +40,29 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link href="./styles/styles.css" rel="stylesheet">
+    <link href="./styles/profil_select.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/e48c77929d.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <?php
-            require('../src/connect.php');
-            // Calcul du nombres de pseudos de l'adresse mail
-            $email = htmlspecialchars($_GET['email']);
-            $requete = $db->query("SELECT COUNT(*) AS nbPseudo FROM profile WHERE email='$email'");
-            $nbDePseudos = $requete->fetch();
-            if ($nbDePseudos[0] < 4) { 
-    ?>
-                <div class="buttons1">
-                    <form method="post">
-                        <!-- /////// NORDINE - START CODE AJOUTER ICI //////// -->
-                        <!-- J'ai ajouter l'image de profil utilisateur relier a la BDD -->
-                        <div class="img_creat_username_box">
-                            <img class="w-25 mb-4 profile-pic-image default_image" src="../images/default_user.jpg" alt="profil" style="cursor: pointer">
-                        </div>
-                        <!-- /////// NORDINE - END CODE AJOUTER ICI //////// -->
-                        <div class="who_are_you_form">
-                            <input type="text" class="Register0_loginForm" name="pseudo" label="Register" id="Register0_loginForm" placeholder="ADD YOUR PSEUDO" /></button><br>
-                            <select class="form-select mb-2" name="categorie" aria-label="Default select example">
-                                <option selected value="adulte">Adulte</option>
-                                <option value="enfant">Enfant</option>
-                            </select>
-                            <div class="third_img_userprofil_choices">
-                                <h1>Choose your profil picture</h1>
-                                <div class="box_picture1">
-                                    <div class="box1">
-                                        <div class="container9">
-                                            <div class="col">
-                                                <div class="row">
-                                                    <input type="radio" name="brandtype" id="4" class="hidetx" value="../images/user_pic/4.png" checked>
-                                                    <label for="4" class="lbl-radio">
-                                                        <div class="display-box">
-                                                            <img src="../images/user_pic/4.png" alt="default image" srcset="">
-                                                        </div>
-                                                        <h4 class="paper-title">Story Bots<h4>
-                                                    </label>
-                                                </div>
-                                                <div class="row">
-                                                    <input type="radio" name="brandtype" id="12" class="hidetx" value="../images/user_pic/12.png">
-                                                    <label for="12" class="lbl-radio">
-                                                        <div class="display-box">
-                                                            <img src="../images/user_pic/12.png" alt="" srcset="">
-                                                        </div>
-                                                        <h4 class="paper-title">Arcane Vi<h4>
-                                                    </label>
-                                                </div>
-                                            </div>
-                                            <div class="col">
-                                                <div class="row">
-                                                    <input type="radio" name="brandtype" id="3" class="hidetx" value="../images/user_pic/3.png">
-                                                    <label for="3" class="lbl-radio">
-                                                        <div class="display-box">
-                                                            <img src="../images/user_pic/3.png" alt="" srcset="">
-                                                        </div>
-                                                        <h4 class="paper-title">Junior<h4>
-                                                    </label>
-                                                </div>
-                                                <div class="row">
-                                                    <input type="radio" name="brandtype" id="9" class="hidetx" value="../images/user_pic/9.png checked">
-                                                    <label for="9" class="lbl-radio">
-                                                        <div class="display-box">
-                                                            <img src="../images/user_pic/9.png" alt="" srcset="">
-                                                        </div>
-                                                        <h4 class="paper-title">Squid game<h4>
-                                                    </label>
-                                                </div>
-                                            </div>
-                                            <div class="col">
-                                                <div class="row">
-                                                    <input type="radio" name="brandtype" id="5" class="hidetx" value="../images/user_pic/5.png">
-                                                    <label for="5" class="lbl-radio">
-                                                        <div class="display-box">
-                                                            <img src="../images/user_pic/5.png" alt="" srcset="">
-                                                        </div>
-                                                        <h4 class="paper-title">Naruto<h4>
-                                                    </label>
-                                                </div>
-                                                <div class="row">
-                                                    <input type="radio" name="brandtype" id="8" class="hidetx" value="../images/user_pic/8.png">
-                                                    <label for="8" class="lbl-radio">
-                                                        <div class="display-box">
-                                                            <img src="../images/user_pic/8.png" alt="" srcset="">
-                                                        </div>
-                                                        <h4 class="paper-title">Omar Sy<h4>
-                                                    </label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- End code choix des image de profil -->
-                            </div>
-                            <button type="submit" class="Register_loginEnter" name="RegisterEnter" label="Register" id="RegisterRegister_loginEnter">CREATE</button>
+    <div class="container1">
+        <h1>Update your profile information</h1>
+        <div id="container_form">
+            <form action='' method="post" id="form_profil">
+                <input type="text" name="pseudo" placeholder="Add your pseudo" value="<?php echo $infosProfil['pseudo']; ?>"></input>
+                <select class="form-select" name="categorie">
+                    <option value="" disabled selected hidden > Select your option</option>
+                    <option value="adulte" <?php if($infosProfil['categorie'] == 'adulte'){ echo "selected";} ?>>Adulte</option>
+                    <option value="enfant" <?php if($infosProfil['categorie'] == 'enfant'){ echo "selected";} ?>>Enfant</option>
+                </select>
+                <button type="submit" name="RegisterEnter" id="subButton">Submit</button>
+            </form>
+                                                <!-- <form action="" method="get">
+                                                    <a class="" href="profil_delete.php?id_pseudo=<?php echo $donnees['id_pseudo'] ?>&email=<?php echo $donnees['email'] ?>" style="">
+                                                    <button>Delete</button></a> 
+                                                    <form> -->
+        </div>
+    </div>
+</body>
 
-                        </div>
-                    </form>
-                </div>
-
-                <small><a href="logout.php">Log Out</a></small>
-            <?php   }
-            $requete = $db->prepare('SELECT * FROM profile WHERE email = ?');
-            $requete->execute(array($_GET['email']));
-            ?>
-                    
-            
-
-<?php
-
-
-
-	if (!empty($_POST['pseudo']))
-	{
-		include('../src/connect.php');
-		// variables
-		$pseudo = htmlspecialchars($_POST['pseudo']);
-        $categorie = htmlspecialchars($_POST['categorie']);
-        $image = htmlspecialchars($_POST['brandtype']);
-		
-		// // sending (ajouter les droits/accès Adulte ou enfant)
-		$req = $db->prepare("INSERT INTO profile(pseudo, categorie, email, image) VALUES (?, ?, ?, ?)");
-        $req->execute(array($pseudo, $categorie, $_GET['email'],$image));
-
-        header('location: profil_select.php?email=' . $_GET['email'] . '');
-        }
-
-?>
+</html>

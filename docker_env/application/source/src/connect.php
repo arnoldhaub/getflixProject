ec2c3aa27da06271bd6525c3497d26437ebafa01
Arnold Haubursin <arnoldhaubursin@gmail.com>
2022-08-16 09:18:06 +0200

    change freeDB to AmazonDB

--- docker_env/application/source/src/connect.php
+++ docker_env/application/source/src/connect.php
@@ -1,5 +1,8 @@
 <?php
+   // DATABASE - FREESQLDATABASE
+   // $db = new PDO('mysql:host=sql11.freesqldatabase.com;dbname=sql11511523;charset=utf8', 'sql11511523', 'i2i73lwhkk');
 
-   $db = new PDO('mysql:host=sql11.freesqldatabase.com;dbname=sql11511523;charset=utf8', 'sql11511523', 'i2i73lwhkk');
+   // DATABASE - AMAZON
+   $db = new PDO('mysql:host=nova.cio2pwzwq6ce.eu-west-3.rds.amazonaws.com;dbname=sql11511523;charset=utf8', 'admin', 'Novaflix2022');
 
 ?>

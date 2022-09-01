24a7a93a91a9f1dcca322dba8b22104f80c39f40
Arnold Haubursin <arnoldhaubursin@gmail.com>
2022-09-01 14:36:21 +0200

    SERIE.PHP - dropdown fixed => works

--- docker_env/application/source/serie.php
+++ docker_env/application/source/serie.php
@@ -44,7 +44,6 @@ include "api/info.php";
         <link href="styles/comments_styles.css" rel="stylesheet">
     </head>
     <body>  
-    <script src="Scripts/umd/popper.min.js"></script>
 <!-----------------------------------------------------------------------
                      HEADER + MENU
 ------------------------------------------------------------------------->
@@ -241,7 +240,7 @@ include "api/info.php";
         </style>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.5/swiper-bundle.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="js/movie_preview_script.js"></script>
         <script src="js/js_comment.js"></script>
